7899f0dca2a807b5bd69bf7880156751a6411a05
Adit2525 <160556720+Adit2525@users.noreply.github.com>
2025-01-05 20:14:48 +0700

    note app adit

---
+++ app/src/main/java/com/example/crud_note_adit/DetailBuku.kt
@@ -0,0 +1,42 @@
+package com.example.crud_note_adit
+
+import android.os.Bundle
+import android.widget.TextView
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.crud_note_adit.R
+
+class DetailBuku : AppCompatActivity() {
+    private lateinit var txtBuku : TextView
+    private lateinit var txtJudulBuku : TextView
+    private lateinit var txtPenulisBuku : TextView
+    private lateinit var txtIsi : TextView
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_detail_buku)
+
+        txtBuku = findViewById(R.id.txtBuku)
+        txtJudulBuku = findViewById(R.id.txtJudulBuku)
+        txtPenulisBuku = findViewById(R.id.txtPenulisBuku)
+        txtIsi = findViewById(R.id.txtIsi)
+
+
+        val judulBuku = intent.getStringExtra("judul")
+        val penulisBuku = intent.getStringExtra("penulis")
+        val isiBuku = intent.getStringExtra("isi")
+
+        txtJudulBuku.setText(judulBuku)
+        txtPenulisBuku.setText(penulisBuku)
+        txtIsi.setText(isiBuku)
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
---
+++ app/src/main/java/com/example/crud_note_adit/model/Modelbuku.kt
@@ -0,0 +1,8 @@
+package com.example.crud_note_adit.model
+
+data class Modelbuku(
+    var id : Int,
+    var judul: String,
+    var penulis : String,
+    var isi : String
+)
